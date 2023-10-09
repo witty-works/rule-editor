90cdb4183e7e9cb88451278779a494a26badf649
Lukas Kahwe Smith <lukas.smith@witty.works>
2023-10-09 21:46:30 +0200

    debug training sentences
    requires https://github.com/witty-works/nlp_api/pull/889

--- rules/admin.py
+++ rules/admin.py
@@ -4,6 +4,11 @@ from django.contrib import admin
 from django import forms
 from django.utils.safestring import mark_safe
 from django.urls import reverse, reverse_lazy
+from django.conf import settings
+
+import requests
+from requests.auth import HTTPBasicAuth
+import json
 
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
@@ -16,6 +21,7 @@ from rangefilter.filter import DateRangeFilter
 from more_admin_filters import MultiSelectRelatedOnlyFilter
 from dal import autocomplete
 from taggit_bulk.actions import tag_wizard
+from dynamic_forms import DynamicField, DynamicFormMixin
 
 from .models import (
     Rule,
@@ -92,13 +98,79 @@ class FalsePositiveInline(admin.StackedInline):
     )
 
 
+def apply_rule(values):
+    if values is None or "rule" not in values or "text" not in values:
+        return None
+
+    url = settings.NLP_API + "/debug/rule"
+
+    auth = (
+        HTTPBasicAuth(settings.NLP_API_USER, settings.NLP_API_PASSWORD)
+        if settings.NLP_API_USER is not None
+        else None
+    )
+
+    rule = Rule.objects.get(pk=values["rule"])
+
+    alternatives = []
+    for alternative in rule.alternatives.all():
+        alternative = {
+            "lemma": alternative.lemma,
+            "word_types": alternative.word_types,
+            "type": str(alternative.type),
+            "pluralization": str(alternative.pluralization),
+            "is_inspiration": alternative.is_inspiration,
+            "is_advanced": alternative.is_advanced,
+        }
+        alternatives.append(alternative)
+
+    false_positives = []
+    for false_positive in rule.false_positives.all():
+        false_positives.append(false_positive.false_positive)
+
+    data = {
+        "text": values["text"],
+        "lang": str(rule.language),
+        "lemma": rule.lemma,
+        "word_types": rule.word_types,
+        "subcategories": rule.diversity_dimension_json,
+        "lower_case": True,
+        "alternatives": alternatives,
+        "false_positives": false_positives,
+    }
+
+    r = requests.post(url, json=data, auth=auth)
+    if r.status_code != 200:
+        body = r.json()
+        return body["detail"] if "detail" in body else r.text
+
+    return r.json()
+
+
+class PrettyJSONEncoder(json.JSONEncoder):
+    def __init__(self, *args, indent, sort_keys, **kwargs):
+        super().__init__(*args, indent=2, sort_keys=True, **kwargs)
+
+
+class TrainingSentenceForm(DynamicFormMixin, forms.ModelForm):
+    response = DynamicField(
+        forms.JSONField,
+        disabled=True,
+        required=False,
+        initial=lambda form: apply_rule(form.initial),
+        encoder=lambda form: PrettyJSONEncoder,
+    )
+
+
 class TrainingSentenceInline(admin.StackedInline):
     model = TrainingSentence
+    form = TrainingSentenceForm
     fields = (
         "text",
         "is_false_positive",
         "is_training_data",
         "comment",
+        "response",
     )
 
 
