236de9b0964ccd93cd15bdd32c4b1db5610fd7f2
Lukas Kahwe Smith <lukas.smith@witty.works>
2023-11-01 11:58:11 +0100

    bump version

--- rule_editor/settings.py
+++ rule_editor/settings.py
@@ -39,7 +39,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 if env("SENTRY_DSN"):
     sentry_sdk.init(
         dsn=env("SENTRY_DSN"),
-        release="0.14.3",
+        release="0.15.0",
         send_default_pii=True,
         integrations=[
             DjangoIntegration(
