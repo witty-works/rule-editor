587f1fb80d02c201259e47bfa11a4644da3edc48
Lukas Kahwe Smith <lukas.smith@witty.works>
2023-11-08 11:39:24 +0100

    do not force '-' alternative on inclusive rules

--- rules/management/commands/import_rules.py
+++ rules/management/commands/import_rules.py
@@ -436,7 +436,7 @@ class Command(BaseCommand):
                         alternative.save()
                         alternative_count += 1
 
-                if alternative_count == 0:
+                if alternative_count == 0 and row["Category"] == "inclusive":
                     alternative = Alternative()
                     alternative.rule = rule
                     alternative.lemma = "-"
